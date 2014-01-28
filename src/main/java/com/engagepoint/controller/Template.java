c6a501fb8b2702f6cacd8a09e4c7042192a2b304
yaroslav-nikolaiko <nikolaiko.yaroslav@gmail.com>
2014-01-28 14:29:03 +0200

    Template.java

---
+++ src/main/java/com/engagepoint/controller/Template.java
@@ -0,0 +1,21 @@
+package com.engagepoint.controller;
+
+/**
+ * Created by yaroslav.nikolaiko on 1/28/14.
+ */
+public class Template {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Template(String name) {
+        this.name = name;
+    }
+}
