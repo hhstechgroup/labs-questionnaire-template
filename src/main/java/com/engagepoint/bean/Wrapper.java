75c8e77804cc8f1f5031563908053e40df0f2fac
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-01-30 14:27:52 +0200

    LQE-48: Added comments to Wrapper class

--- src/main/java/com/engagepoint/bean/Wrapper.java
+++ src/main/java/com/engagepoint/bean/Wrapper.java
@@ -4,6 +4,12 @@ import javax.xml.bind.annotation.XmlAnyElement;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class for wrapping root element.
+ * With this class we needn't create separate class for root element
+ *
+ * @param <T> class to wrap
+ */
 public class Wrapper<T> {
     private List<T> items;
 
@@ -15,6 +21,9 @@ public class Wrapper<T> {
         this.items = items;
     }
 
+    /**
+     * @return List of type T elements
+     */
     @XmlAnyElement(lax = true)
     public List<T> getItems() {
         return items;
