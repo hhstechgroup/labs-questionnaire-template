bda3ebb835e15cd85d2b07389730dab1f5600b67
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-01-30 12:51:12 +0200

    LQE-48: Deleted unnecessary

--- src/main/java/com/engagepoint/xmlparser/QuestionnaireForms.java
+++ src/main/java/com/engagepoint/xmlparser/QuestionnaireForms.java
@@ -1,19 +1,21 @@
 package com.engagepoint.xmlparser;
 
+import com.engagepoint.bean.TemplateBean;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
 @XmlRootElement(name = "questionnaire-forms")
 public class QuestionnaireForms {
-    private List<QuestionnaireForm> questionnaireFormList;
+
+    private List<TemplateBean> questionnaireFormList;
 
     @XmlElement(name = "questionnaire-form")
-    public List<QuestionnaireForm> getQuestionnaireFormList() {
+    public List<TemplateBean> getQuestionnaireFormList() {
         return questionnaireFormList;
     }
-
-    public void setQuestionnaireFormList(List<QuestionnaireForm> questionnaireFormList) {
+    public void setQuestionnaireFormList(List<TemplateBean> questionnaireFormList) {
         this.questionnaireFormList = questionnaireFormList;
     }
+
 }
