2ccc4ee840b75fb3e001dd81c3c32b10abeff1ab
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-01-30 14:15:34 +0200

    LQE-48: added class

---
+++ src/main/java/com/engagepoint/bean/Wrapper.java
@@ -0,0 +1,22 @@
+package com.engagepoint.bean;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wrapper<T> {
+    private List<T> items;
+
+    public Wrapper() {
+        items = new ArrayList<T>();
+    }
+
+    public Wrapper(List<T> items) {
+        this.items = items;
+    }
+
+    @XmlAnyElement(lax = true)
+    public List<T> getItems() {
+        return items;
+    }
+}
