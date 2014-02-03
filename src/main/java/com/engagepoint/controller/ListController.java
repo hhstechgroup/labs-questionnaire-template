95280017955d4ff6f4d52261de62eec0ae57bb42
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-02-03 16:37:52 +0200

    LQE-65: Added comments

--- src/main/java/com/engagepoint/controller/ListController.java
+++ src/main/java/com/engagepoint/controller/ListController.java
@@ -19,16 +19,15 @@ public class ListController implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private List<TemplateBean> list;
+    private List<TemplateBean> filteredList;
+    private String xmlPath;
     //TODO
     // 1. adding to filtered list and adding to main list must be together in one method (removing too)
     // 2. in the same method must be list sorting
     // 3. there is a bug: when list is filtered, after editing or adding filter (the field) becomes empty
-    private List<TemplateBean> filteredList;
-    private String xmlPath;
 
     public ListController() {
         list = new ArrayList<TemplateBean>();
-        //TODO
         //searching path of XML file in glassfish
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         xmlPath = classLoader.getResource("Questionnaire.xml").getPath();
@@ -55,18 +54,30 @@ public class ListController implements Serializable {
         this.filteredList = filteredList;
     }
 
-    //operations on both lists
+    /**
+     * Add template.
+     *
+     * @param template template to be added
+     */
     public void addTemplateToList(TemplateBean template) {
         this.list.add(template);
     }
 
+    /**
+     * Sort list of templates.
+     */
     public void sort() {
         Collections.sort(list);
         if (filteredList != null)
             Collections.sort(filteredList);
     }
 
-    //operations on current template
+    /**
+     * Clone template.
+     *
+     * @param template template to be clonned
+     * @throws CloneNotSupportedException
+     */
     public void clone(TemplateBean template) throws CloneNotSupportedException {
         TemplateBean newTemplate = (TemplateBean) template.clone();
         list.add(newTemplate);
@@ -76,10 +87,9 @@ public class ListController implements Serializable {
     }
 
     /**
-     * deleting Template from list
+     * Delete template from list.
      *
-     * @param template to be deleted
-     * @author dmytro.sorych
+     * @param template template to be deleted
      */
     public void delete(TemplateBean template) {
         list.remove(template);
@@ -87,24 +97,28 @@ public class ListController implements Serializable {
             filteredList.remove(template);
     }
 
-    //exporting/importing XML
+
+    /**
+     * Perform export questionnaire to XML file.
+     */
     public void exportToXML() {
+        //TODO
         addMessage("Data exported");
     }
 
+    /**
+     * Perform import questionnaire from XML file.
+     */
     public void importFromXML() {
+        //TODO
         addMessage("Data imported");
-        list.addAll(XmlImportExport.importXmlTemplate(xmlPath));
     }
 
-    //messaging
-
     /**
-     * showing message on delete popup
+     * Show message on delete pop-up.
      *
-     * @param template
-     * @return String message to display
-     * @author dmytro.sorych
+     * @param template template to delete
+     * @return message to display
      */
     public String getMessageOnDelete(TemplateBean template) {
         if (template == null)
@@ -114,12 +128,21 @@ public class ListController implements Serializable {
         return "Please confirm deleting of " + template.getTemplateName();
     }
 
+    /**
+     * Show message on default page.
+     *
+     * @param summary message content
+     */
     private void addMessage(String summary) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    //income
+    /**
+     * Get page name and perform redirect.
+     *
+     * @return page name
+     */
     public String income() {
         return "/index?faces-redirect=true";
     }
