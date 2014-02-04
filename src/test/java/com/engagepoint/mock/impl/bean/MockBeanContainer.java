f132e1e9e6599258f8203219568c88835605c55a
yaroslav-nikolaiko <nikolaiko.yaroslav@gmail.com>
2014-02-04 15:21:09 +0200

    LQE-91 Mock Beans add Conteiners

---
+++ src/test/java/com/engagepoint/mock/impl/bean/MockBeanContainer.java
@@ -0,0 +1,23 @@
+package com.engagepoint.mock.impl.bean;
+
+import com.engagepoint.mock.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockBeanContainer {
+    List<MockBean> list = new ArrayList<MockBean>();
+
+    public MockBeanContainer(){
+        list.add(new AllNullMockBean());
+        list.add(new NotNullBeans());
+    }
+    public void add(MockBean bean){
+        list.add(bean);
+    }
+
+    public List<MockBean> getList(){
+        return list;
+    }
+
+}
---
+++ src/test/java/com/engagepoint/mock/impl/list/MockBeansListContainer.java
@@ -0,0 +1,24 @@
+package com.engagepoint.mock.impl.list;
+
+import com.engagepoint.mock.MockBeansList;
+import com.engagepoint.mock.impl.bean.NotNullBeans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockBeansListContainer {
+    List<MockBeansList> list = new ArrayList<MockBeansList>();
+
+    public MockBeansListContainer(){
+        list.add(new AllNullMockBeansList());
+        list.add(new OneNullOneRealBeansList());
+    }
+    public void add(MockBeansList bean){
+        list.add(bean);
+    }
+
+    public List<MockBeansList> getList(){
+        return list;
+    }
+
+}
