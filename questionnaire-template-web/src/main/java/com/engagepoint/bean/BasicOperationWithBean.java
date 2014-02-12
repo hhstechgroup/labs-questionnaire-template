77e7620b2bb9b98150db60f7d4c62ebbe720b7f1
yurii.kukharskyi <yurii.kukharskyi@gmail.com>
2014-02-12 18:38:37 +0200

    LQE-145: Ready for demo

--- questionnaire-template-web/src/main/java/com/engagepoint/bean/BasicOperationWithBean.java
+++ questionnaire-template-web/src/main/java/com/engagepoint/bean/BasicOperationWithBean.java
@@ -1,8 +1,11 @@
 package com.engagepoint.bean;
 
+/*
+ * Basic operation with beans
+ * Have methods need fot beans high level
+ * (beans who contains lis of include beans)
+ */
 public interface BasicOperationWithBean {
     void deleteFromInnerList(Object o);
     void addToInnerList(Object o);
-    @Override
-    String toString();
 }
