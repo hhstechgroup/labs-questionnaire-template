f34761849bd22e88dc7c36716889a242da4de4b4
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-02-17 17:38:37 +0200

    LQE-154: Added bean

---
+++ questionnaire-template-web/src/main/java/com/engagepoint/bean/OptionBean.java
@@ -0,0 +1,12 @@
+package com.engagepoint.bean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * Created by kostiantyn.ustinov on 2/17/14.
+ */
+@Named
+@RequestScoped
+public class OptionBean extends QuestionBean {
+}
