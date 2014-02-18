d4de7cb39a3c5ef88a7da407de79e64a33604613
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-02-18 12:34:04 +0200

    LQE-154: Repaired class for selecting

--- questionnaire-template-web/src/main/java/com/engagepoint/model/OptionDataModel.java
+++ questionnaire-template-web/src/main/java/com/engagepoint/model/OptionDataModel.java
@@ -1,33 +1,37 @@
 package com.engagepoint.model;
 
-import com.engagepoint.bean.TemplateBean;
 import org.primefaces.model.SelectableDataModel;
 
 import javax.faces.model.ListDataModel;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: anna.zagrebelnaya
- * Date: 2/4/14
- * Time: 11:49 AM
- * To change this template use File | Settings | File Templates.
+ * Class for enabling selection in the dataTable.
  */
+public class OptionDataModel extends ListDataModel<VariantItem> implements SelectableDataModel<VariantItem> {
 
-
-public class OptionDataModel extends ListDataModel<String> implements SelectableDataModel<String> {
-
-    public OptionDataModel(List<String> data) {
+    public OptionDataModel(List<VariantItem> data) {
         super(data);
     }
 
     @Override
-    public String getRowData(String optionTitle) {
-        return optionTitle;
+    public Object getRowKey(VariantItem object) {
+        return object.getValue();
     }
 
+    /**
+     * Get row from table by variant value.
+     *
+     * @param rowKey variant value
+     * @return row data (VariantItem object)
+     */
     @Override
-    public Object getRowKey(String optionTitle) {
-        return optionTitle;
+    public VariantItem getRowData(String rowKey) {
+        List<VariantItem> itemList = (List<VariantItem>) getWrappedData();
+        for (VariantItem variantItem : itemList) {
+            if (variantItem.getValue().equals(rowKey))
+                return variantItem;
+        }
+        return null;
     }
 }
