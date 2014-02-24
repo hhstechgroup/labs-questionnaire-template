f566567cb9891635289aaf73f5109bd744f117f0
anton.kovunov <anton.kovunov@gmail.com>
2014-02-24 16:36:41 +0200

    Clone test made

--- labs-questionnaire-template-acceptancetest/src/test/java/com/engagepoint/acceptancetest/LQE23.java
+++ labs-questionnaire-template-acceptancetest/src/test/java/com/engagepoint/acceptancetest/LQE23.java
@@ -1,7 +1,8 @@
 package com.engagepoint.acceptancetest;
 
+import net.thucydides.jbehave.ThucydidesJUnitStory;
+
 /**
  * Created by anton.kovunov on 2/24/14.
  */
-public class LQE23 {
-}
+public class LQE23 extends ThucydidesJUnitStory {}
