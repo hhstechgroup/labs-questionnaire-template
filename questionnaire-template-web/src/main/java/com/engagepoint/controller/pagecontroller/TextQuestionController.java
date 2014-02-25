2c11a93ed5682081cbb3ad01ef0eb4791a70bd0a
oleksandr.koval <oleksandr.koval@engagepoint.com>
2014-02-25 12:14:42 +0200

    LQE-207

---
+++ questionnaire-template-web/src/main/java/com/engagepoint/controller/pagecontroller/TextQuestionController.java
@@ -0,0 +1,26 @@
+package com.engagepoint.controller.pagecontroller;
+
+
+import com.engagepoint.bean.QuestionBeans.QuestionBean;
+import com.engagepoint.bean.QuestionBeans.TextQuestionBean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class TextQuestionController {
+
+    @Inject
+    private QuestionEditController questionController;
+
+    public String getDefaultAnswer() {
+        return "";
+    }
+
+    public void setDefaultAnswer(String defaultAnswer) {
+        ((TextQuestionBean)this.questionController.getCurrentQuestion()).setDefaultAnswer(defaultAnswer);
+    }
+
+}
