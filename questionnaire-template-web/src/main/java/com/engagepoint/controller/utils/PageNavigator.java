340aa1831587972f102d40a6b298fca36fcef3db
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-02-28 12:09:07 +0200

    LQE-217: Added page navigator class

--- questionnaire-template-web/src/main/java/com/engagepoint/controller/utils/PageNavigator.java
+++ questionnaire-template-web/src/main/java/com/engagepoint/controller/utils/PageNavigator.java
@@ -6,14 +6,14 @@ public class PageNavigator {
     public static final String QUESTION_EDIT_PAGE = "/pages/questionEdit?faces-redirect=true&includeViewParams=true";
     public static final String TEMPLATE_EDIT_PAGE = "/pages/templateEdit?faces-redirect=true&includeViewParams=true";
     //question pages
-    public static final String STAB_PAGE = "/question-pages/stab.xhtml";
-    public static final String NOT_CHOOSE_QUESTION_PAGE = "/question-pages/notChooseQuestion.xhtml";
-    public static final String TEXT_QUESTION_PAGE = "/question-pages/textQuestion.xhtml";
-    public static final String DATE_QUESTION_PAGE = "/question-pages/dateQuestion.xhtml";
-    public static final String RANGE_QUESTION_PAGE = "/question-pages/rangeQuestion.xhtml";
-    public static final String TIME_QUESTION_PAGE = "/question-pages/timeQuestion.xhtml";
-    public static final String PARAGRAPH_TEXT_QUESTION_PAGE = "/question-pages/paragraphtextQuestion.xhtml";
-    public static final String CHOOSE_FROM_LIST_QUESTION_PAGE = "/question-pages/chooseFromListQuestion.xhtml";
-    public static final String FILE_UPLOAD_QUESTION_PAGE = "/question-pages/fileUploadQuestion.xhtml";
-    public static final String CHECKBOX_QUESTION_PAGE = "/question-pages/checkBoxQuestion.xhtml";
+    public static final String STAB_PAGE = "/pages/question-pages/stab.xhtml";
+    public static final String NOT_CHOOSE_QUESTION_PAGE = "/pages/question-pages/notChooseQuestion.xhtml";
+    public static final String TEXT_QUESTION_PAGE = "/pages/question-pages/textQuestion.xhtml";
+    public static final String DATE_QUESTION_PAGE = "/pages/question-pages/dateQuestion.xhtml";
+    public static final String RANGE_QUESTION_PAGE = "/pages/question-pages/rangeQuestion.xhtml";
+    public static final String TIME_QUESTION_PAGE = "/pages/question-pages/timeQuestion.xhtml";
+    public static final String PARAGRAPH_TEXT_QUESTION_PAGE = "/pages/question-pages/paragraphtextQuestion.xhtml";
+    public static final String CHOOSE_FROM_LIST_QUESTION_PAGE = "/pages/question-pages/chooseFromListQuestion.xhtml";
+    public static final String FILE_UPLOAD_QUESTION_PAGE = "/pages/question-pages/fileUploadQuestion.xhtml";
+    public static final String CHECKBOX_QUESTION_PAGE = "/pages/question-pages/checkBoxQuestion.xhtml";
 }
