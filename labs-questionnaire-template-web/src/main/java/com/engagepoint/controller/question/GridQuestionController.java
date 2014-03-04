2d16bb2f8e12ca12b945e0455687ee36a6b3e251
dmytro.sorych <dmytro.sorych@lab12.engagepoint-lab.local>
2014-03-04 14:14:55 +0200

    added files for previous commit

---
+++ labs-questionnaire-template-web/src/main/java/com/engagepoint/controller/question/GridQuestionController.java
@@ -0,0 +1,168 @@
+package com.engagepoint.controller.question;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.engagepoint.controller.page.QuestionEditController;
+import com.engagepoint.controller.page.TemplateEditController;
+import com.engagepoint.model.question.GridQuestionBean;
+import com.engagepoint.model.question.QuestionBean;
+import com.engagepoint.model.question.utils.VariantItem;
+import com.engagepoint.model.table.ListOfOptionsDataModel;
+
+@Named
+@ConversationScoped
+public class GridQuestionController extends QuestionEditController {
+
+	private GridQuestionBean currentQuestion;
+
+	// dataModel for table
+	private ListOfOptionsDataModel dataModel;
+	// dataModel for second table
+	private ListOfOptionsDataModel dataModel2;
+
+	@Inject
+	private TemplateEditController templateEditController;
+
+	@Inject
+	private Conversation conversation;
+
+	@PostConstruct
+	public void postConstruct() {
+		beginConversation();
+		QuestionBean questionBean = getTemplateTreeController()
+				.getCurrentQuestion(); // TODO duble edit
+		if (questionBean == null) {
+			setNew(true);
+			currentQuestion = new GridQuestionBean();
+			currentQuestion.setQuestionType(templateEditController
+					.getSelectedQuestionType());
+			dataModel = new ListOfOptionsDataModel();
+			dataModel2 = new ListOfOptionsDataModel();
+		} else {
+			currentQuestion = (GridQuestionBean) questionBean;
+			dataModel = new ListOfOptionsDataModel(currentQuestion.getOptions());
+			dataModel2 = new ListOfOptionsDataModel(
+					currentQuestion.getOptions2());
+		}
+	}
+
+	public void beginConversation() {
+		if (conversation.isTransient()) {
+			conversation.begin();
+		}
+	}
+
+	public void endConversation() {
+		if (!conversation.isTransient()) {
+			conversation.end();
+		}
+	}
+
+	public ListOfOptionsDataModel getDataModel2() {
+		return dataModel2;
+	}
+
+	public void setDataModel2(ListOfOptionsDataModel dataModel2) {
+		this.dataModel2 = dataModel2;
+	}
+
+	public GridQuestionBean getCurrentQuestion() {
+		return currentQuestion;
+	}
+
+	public void setCurrentQuestion(GridQuestionBean currentQuestion) {
+		this.currentQuestion = currentQuestion;
+	}
+
+	public ListOfOptionsDataModel getDataModel() {
+		return dataModel;
+	}
+
+	public void setDataModel(ListOfOptionsDataModel dataModel) {
+		this.dataModel = dataModel;
+	}
+
+	/**
+	 * Update options in ListOfOptionsDataModel.
+	 */
+	private void updateModel() {
+		getDataModel().setWrappedData(getCurrentQuestion().getOptions());
+	}
+
+	/**
+	 * Update options2 in ListOfOptionsDataModel.
+	 */
+	private void updateModel2() {
+		getDataModel2().setWrappedData(getCurrentQuestion().getOptions2());
+	}
+
+	/**
+	 * Add variant to a question.
+	 * 
+	 * @param option
+	 *            VariantItem object
+	 */
+	public void addOption(String option) {
+		getCurrentQuestion().getOptions().add(new VariantItem("New"));
+		updateModel();
+	}
+
+	/**
+	 * Add variant to a question for second table.
+	 * 
+	 * @param option
+	 *            VariantItem object
+	 */
+	public void addOption2(String option) {
+		getCurrentQuestion().getOptions2().add(new VariantItem("New"));
+		updateModel2();
+	}
+
+	/**
+	 * Remove variant from a question.
+	 * 
+	 * @param option
+	 *            VariantItem object
+	 */
+	public void removeOption(VariantItem option) {
+		getCurrentQuestion().getOptions().remove(option);
+		updateModel();
+	}
+
+	/**
+	 * Remove variant from a question.
+	 * 
+	 * @param option
+	 *            VariantItem object
+	 */
+	public void removeOption2(VariantItem option) {
+		getCurrentQuestion().getOptions2().remove(option);
+		updateModel2();
+	}
+
+	@Override
+	public String actionSave() {
+		try {
+			currentQuestion.setOptions((ArrayList<VariantItem>) dataModel.getWrappedData());
+			currentQuestion.setOptions2((ArrayList<VariantItem>) dataModel2.getWrappedData());
+		} catch (ClassCastException e) {
+			// TODO
+		}
+		getTemplateTreeController().setCurrentQuestion(currentQuestion);
+		endConversation();
+		return super.actionSave();
+	}
+
+	@Override
+	public String actionCancel() {
+		endConversation();
+		return super.actionCancel();
+	}
+
+}
---
+++ labs-questionnaire-template-web/src/main/java/com/engagepoint/model/question/GridQuestionBean.java
@@ -0,0 +1,53 @@
+package com.engagepoint.model.question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.engagepoint.model.question.utils.VariantItem;
+
+public class GridQuestionBean extends OptionsQuestionBean {
+	
+	//list of variants for second table
+	private List<VariantItem> options2;
+	//selected variants for second table
+    private List<VariantItem> defaultOptions2; 	//TODO maybe only defaultOption2 is necessary
+    //selected variant for second table
+    private VariantItem defaultOption2; 		//TODO maybe only defaultOptions2 is necessary
+
+    public GridQuestionBean() {
+        this.options2 = new ArrayList<VariantItem>();
+    }
+	
+	public List<VariantItem> getOptions2() {
+		return options2;
+	}
+
+	public void setOptions2(List<VariantItem> options2) {
+		this.options2 = options2;
+	}
+
+	public List<VariantItem> getDefaultOptions2() {
+		return defaultOptions2;
+	}
+
+	public void setDefaultOptions2(List<VariantItem> defaultOptions2) {
+		this.defaultOptions2 = defaultOptions2;
+	}
+
+	public VariantItem getDefaultOption2() {
+		return defaultOption2;
+	}
+
+	public void setDefaultOption2(VariantItem defaultOption2) {
+		this.defaultOption2 = defaultOption2;
+	}
+	
+	@Override
+    public Object clone() throws CloneNotSupportedException {
+		GridQuestionBean copy = (GridQuestionBean) super.clone();
+        copy.setOptions2(this.options2);
+        copy.setDefaultOption2(this.defaultOption2);
+        return copy;
+    }
+
+}
