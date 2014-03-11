e5af447ab5f3036be4623103a06ca873ac8f09ba
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-03-11 15:52:31 +0200

    LQE-251

--- labs-questionnaire-template-web/src/main/java/com/engagepoint/controller/page/QuestionEditController.java
+++ labs-questionnaire-template-web/src/main/java/com/engagepoint/controller/page/QuestionEditController.java
@@ -1,7 +1,6 @@
 package com.engagepoint.controller.page;
 
 import com.engagepoint.controller.utils.PageNavigator;
-import com.engagepoint.model.question.rules.RulesContainer;
 
 import java.io.Serializable;
 import javax.inject.Inject;
@@ -11,38 +10,16 @@ import javax.inject.Inject;
  */
 public abstract class QuestionEditController implements Serializable {
     private boolean isNew;
-    //contains all rules
-    private RulesContainer rulesContainer;
-    //show add rule button
-    private boolean addRuleButtonIsVisible;
-    //show cancel rule edition button
-    private boolean cancelRuleEditionButtonIsVisible;
-    //show rules table
-    private boolean addRulesTableIsVisible;
-    //show question id list
-    private boolean chooseDependentQuestionListVisible;
 
     @Inject
     private TemplateTreeController templateTreeController;
 
     public QuestionEditController() {
-        rulesContainer = new RulesContainer();
-        addRuleButtonIsVisible = true;
-        isNew = false;
-    }
-
-    public RulesContainer getRulesContainer() {
-        return rulesContainer;
-    }
-
-    public void setRulesContainer(RulesContainer rulesContainer) {
-        this.rulesContainer = rulesContainer;
     }
 
     public TemplateTreeController getTemplateTreeController() {
         return templateTreeController;
     }
-
 
     public boolean isNew() {
         return isNew;
@@ -61,61 +38,5 @@ public abstract class QuestionEditController implements Serializable {
 
     public String actionCancel() {
         return PageNavigator.TEMPLATE_EDIT_PAGE;
-    }
-
-    public boolean isAddRulesTableIsVisible() {
-        return addRulesTableIsVisible;
-    }
-
-    public void setAddRulesTableIsVisible(boolean addRulesTableIsVisible) {
-        this.addRulesTableIsVisible = addRulesTableIsVisible;
-    }
-
-    public boolean isCancelRuleEditionButtonIsVisible() {
-        return cancelRuleEditionButtonIsVisible;
-    }
-
-    public void setCancelRuleEditionButtonIsVisible(boolean cancelRuleEditionButtonIsVisible) {
-        this.cancelRuleEditionButtonIsVisible = cancelRuleEditionButtonIsVisible;
-    }
-
-    public boolean isAddRuleButtonIsVisible() {
-        return addRuleButtonIsVisible;
-    }
-
-    public void setAddRuleButtonIsVisible(boolean addRuleButtonIsVisible) {
-        this.addRuleButtonIsVisible = addRuleButtonIsVisible;
-    }
-
-    public boolean isChooseDependentQuestionListVisible() {
-        return chooseDependentQuestionListVisible;
-    }
-
-    public void setChooseDependentQuestionListVisible(boolean chooseDependentQuestionListVisible) {
-        this.chooseDependentQuestionListVisible = chooseDependentQuestionListVisible;
-    }
-
-    /**
-     * Set elements visibility after add rule button was clicked.
-     */
-    public void addRuleAction() {
-        setAddRuleButtonIsVisible(false);
-        setCancelRuleEditionButtonIsVisible(true);
-        setAddRulesTableIsVisible(true);
-    }
-
-    /**
-     * Set elements visibility after cancel rule button was clicked.
-     */
-    public void cancelAddRuleAction() {
-        setChooseDependentQuestionListVisible(false);
-        setCancelRuleEditionButtonIsVisible(false);
-        setAddRulesTableIsVisible(false);
-        setAddRuleButtonIsVisible(true);
-    }
-
-    public void createRule(String ruleName) {
-        setAddRulesTableIsVisible(false);
-        setChooseDependentQuestionListVisible(true);
     }
 }
