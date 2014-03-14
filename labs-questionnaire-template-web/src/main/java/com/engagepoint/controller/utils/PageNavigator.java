e19de1440ac584d7954e96bf581b318cfac01b1c
yurii.kukharskyi <yurii.kukharskyi@gmail.com>
2014-03-14 12:49:42 +0200

    LQE-253: some changes

--- labs-questionnaire-template-web/src/main/java/com/engagepoint/controller/utils/PageNavigator.java
+++ labs-questionnaire-template-web/src/main/java/com/engagepoint/controller/utils/PageNavigator.java
@@ -16,5 +16,5 @@ public class PageNavigator {
     public static final String CHOOSE_FROM_LIST_QUESTION_PAGE = "/pages/question-pages/chooseFromListQuestion.xhtml?faces-redirect=true&includeViewParams=true";
     public static final String FILE_UPLOAD_QUESTION_PAGE = "/pages/question-pages/fileUploadQuestion.xhtml?faces-redirect=true&includeViewParams=true";
     public static final String CHECKBOX_QUESTION_PAGE = "/pages/question-pages/checkBoxQuestion.xhtml?faces-redirect=true&includeViewParams=true";
-    public static final String GRID_QUESTION_PAGE = "/pages/question-pages/gridQuestion.xhtml?faces-redirect=true";
+    public static final String GRID_QUESTION_PAGE = "/pages/question-pages/gridQuestion.xhtml?faces-redirect=true&includeViewParams=true";
 }
