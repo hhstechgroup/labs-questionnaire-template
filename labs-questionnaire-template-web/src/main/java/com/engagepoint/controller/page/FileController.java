6315b90dd950058ec71fdb7ddaaa866d47050545
Kostya Ustinov <kostiantyn.ustinov@engagepoint.com>
2014-03-14 12:03:40 +0200

    LQE-267: refactor

--- labs-questionnaire-template-web/src/main/java/com/engagepoint/controller/page/FileController.java
+++ labs-questionnaire-template-web/src/main/java/com/engagepoint/controller/page/FileController.java
@@ -6,18 +6,11 @@ import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import org.primefaces.model.UploadedFile;
 
-import javax.faces.bean.RequestScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.io.*;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: anna.zagrebelnaya
- * Date: 2/7/14
- * Time: 5:15 PM
- * To change this template use File | Settings | File Templates.
- */
 @Named
 @RequestScoped
 public class FileController implements Serializable {
@@ -26,20 +19,20 @@ public class FileController implements Serializable {
     private StreamedContent downloadFile;
     private UploadedFile uploadFile;
 
-    public static void setPathToTempFile(String pathToTempFile) {
-        FileController.pathToTempFile = pathToTempFile;
-    }
-
     public FileController() throws IOException {
-
-        if (pathToTempFile.isEmpty())
+        if (pathToTempFile.isEmpty()) {
             return;
+        }
         File file = new File(pathToTempFile);
         file.deleteOnExit();
         InputStream stream = new FileInputStream(file);
         if (pathToTempFile.contains("xml")) {
             downloadFile = new DefaultStreamedContent(stream, "application/xml", "ExportedTemplates.xml");
         }
+    }
+
+    public static void setPathToTempFile(String pathToTempFile) {
+        FileController.pathToTempFile = pathToTempFile;
     }
 
     public StreamedContent getDownloadFile() {
@@ -58,10 +51,9 @@ public class FileController implements Serializable {
      * Create temporary XML file with many templates.
      */
     public static File createTempXml(List<TemplateBean> list) throws IOException {
-        File tmpFile = File.createTempFile("temp",".xml");
+        File tmpFile = File.createTempFile("temp", ".xml");
         XmlImportExport.exportXmlTemplates(list, tmpFile.getPath());
         tmpFile.deleteOnExit();
         return tmpFile;
     }
-
 }
